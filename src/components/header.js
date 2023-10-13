c4d814cd7d038e2848933ef5b653af22a4c9e6c2
JIAHLEE0213 <ja7943@naver.com>
2023-10-13 22:34:03 +0900

    feat:Header 컴포넌트 완성

---
+++ src/components/header.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Header() {
+  const categoryItems = [
+    { label: 'ABOUT', id: 'contact' },
+    { label: 'PROJECT', id: 'project' },
+    { label: 'SKILL', id: 'skill' },
+  ];
+
+  return (
+    <HeaderStyle>
+      <div className="header">
+        <div className="header-logo">JIAH</div>
+        <ul className="header-category">
+          {categoryItems.map((item) => (
+            <CategoryItem key={item.id} className="header-categoty-items">
+              {item.label}
+            </CategoryItem>
+          ))}
+        </ul>
+      </div>
+    </HeaderStyle>
+  );
+}
+
+const HeaderStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 64px;
+  padding: 0px 24px;
+  margin: 10px 24px;
+  .header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+  }
+  .header-logo {
+    display: flex;
+    flex-direction: row;
+    font-size: 1.75rem;
+    font-weight: 500;
+    padding-left: 36px;
+    cursor: pointer;
+  }
+  .header-category {
+    display: flex;
+    flex-direction: row;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const CategoryItem = styled.li`
+  display: flex;
+  padding: 0px 8px;
+  font-size: 1rem;
+  font-weight: 500;
+  align-items: center;
+  cursor: pointer;
+`;
