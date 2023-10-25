dae7025b1df0e71c144a7bcde642c5513cf7cf45
JIAHLEE0213 <ja7943@naver.com>
2023-10-25 16:51:29 +0900

    feat:project 배열 생성

---
+++ src/constants/projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Projects = [
+  {
+    img: (
+      // eslint-disable-next-line no-undef
+      <img src={process.env.PUBLIC_URL + '/img/Celebee.png'} alt="Celebee" />
+    ),
+    title: 'Celebee',
+    subtitle:
+      '나와 같은 관심사를 가진 친구를 찾기 위해 모임 생성 및 참여를 할 수 있는 페이지를 개발하였습니다.',
+  },
+];
