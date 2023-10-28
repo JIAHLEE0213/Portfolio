9b364c3f8170fde8383ca91be7460ae5cbe84cd3
JIAHLEE0213 <ja7943@naver.com>
2023-10-28 23:35:47 +0900

    feat:페이지 연결

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import Project from './section/project';
 import Header from './components/header';
-// import Detail from './components/detail';
-// import Project from './section/project';
-// import Intro from './section/Intro';
-// import About from './section/about';
-// import Skill from './section/skill';
+import Main from './pages/main';
+import Intro from './section/Intro';
 
 export default function App() {
   return (
     <>
       <Header />
-      {/* <Intro /> */}
-      {/* <About /> */}
-      {/* <Skill /> */}
-      <Project />
-      {/* <Detail /> */}
+      <Intro />
+      <Main />
     </>
   );
 }
